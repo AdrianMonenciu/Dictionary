6db3fecf2e54860505692ca6f72369262bbaf7bd
AdrianMonenciu <101457701+AdrianMonenciu@users.noreply.github.com>
2022-03-16 22:11:08 +0000

    Release 1

--- Dictionary.js
+++ Dictionary.js
@@ -1,77 +1,22 @@
-const cars = [];
-cars[0]= "Saab";
-cars[1]= "Volvo";
-cars[2]= "BMW";
-
-//const fruits = ["Banana", "Orange", "Apple"];
-//fruits.push("Lemon");  // Adds a new element (Lemon) to fruits
-
-
-//document.getElementById("demo").innerHTML = ages.find(checkAge);
-
-
-
-//const ages = [3, 10, 18, 20];
-
-//function checkAge(age) {
-//  return age > 18;
-//}
-
-//function myFunction() {
-//  document.getElementById("demo").innerHTML = ages.find(checkAge);
-//}
-
-
+var wordsArr = []
 
 function addWord() {
-  //alert("test test")
   var newWord = document.getElementById("newWord").value
-  alert(newWord)
-  cars.push(newWord)
-  alert(cars[3])
-}
-
-
-var searchedWord
-
-function searchWord() {
-  searchedWord = document.getElementById("searchedWord").value
-  alert(searchedWord)
-  //salert(cars.find(checkWord)
-  var found = cars.includes(searchedWord);
-  alert(found)
-}
-
-
-
-
-var randomNr
-function luckyButton(clicked_id) {
-  if (clicked_id == randomNr) {
-    alert("You clicked the lucky button!")
+  if (wordsArr.includes(newWord)) {
+    alert("The word: '" + newWord + "' is already in the dictionary, please try another one.")
   } else {
-    alert("You didn't click the lucky button! The lucky button is: " + randomNr)
+    wordsArr.push(newWord)
   }
+  //alert(wordsArr[wordsArr.length - 1])
+  document.getElementById("newWord").value = ''
 }
 
-
-function addButtons() {
-  var buttonsNr = document.getElementById("buttonsNr").value
-  if (buttonsNr < 2) {
-    alert("Minimum buttons nr is 2, the game will restart.")
-    restartTheGame();
+function checkWord() {
+  var checkedWord = document.getElementById("checkedWord").value
+  if (wordsArr.includes(checkedWord)) {
+    alert("The word: '" + checkedWord + "' is already in the dictionary.")
   } else {
-    randomNr = Math.floor((Math.random() * buttonsNr) + 1)
-    submitButton.disabled = true
-    for (let i = 1; i <= buttonsNr; i++) {
-      buttonsGroup.innerHTML += ('\
-        <button type="button" class="btn btn-outline-primary" id="' + i + '" onclick="luckyButton(this.id)">Button ' + i + '</button>\
-      ')
-    }
+    alert("The word: '" + checkedWord + "' is not found in the dictionary.")
   }
-  return false;
-}
-
-function restartTheGame() {
-  window.location.reload()
+  document.getElementById("checkedWord").value = ''
 }
